596363c46c2157bf8d693bed693566b56b39a069
solasolo8 <solveig.asbjornsen@gmail.com>
2024-10-14 20:04:39 +0900

    Update comfy_provisioning_basic_ipa.sh

--- comfy_provisioning_basic_ipa.sh
+++ comfy_provisioning_basic_ipa.sh
@@ -28,6 +28,7 @@ NODES=(
     "https://github.com/twri/sdxl_prompt_styler"
     "https://github.com/pythongosssss/ComfyUI-WD14-Tagger"
     "https://github.com/LykosAI/ComfyUI-Inference-Core-Nodes"
+    "https://github.com/LykosAI/ComfyUI-Inference-Core-Nodes/tree/main"
     
     "https://github.com/cubiq/ComfyUI_IPAdapter_plus"
 
